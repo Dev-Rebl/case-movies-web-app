dd6951c68cb62400bc471414b793271180aa6bd8
DevRebl <marc@smalbrugge.com>
2022-11-21 14:51:46 +0100

    Configure base styling variables and mixins

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,3 @@
 declare module '*.scss';
+
+declare var __dirname;
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@styles': resolve(__dirname, 'src/styles'),
+    },
+  },
   plugins: [
     react(),
     tsconfigPaths()
