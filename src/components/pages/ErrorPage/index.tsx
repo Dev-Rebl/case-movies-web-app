235f1ab792640532dab22f525dcbe551be4d6604
DevRebl <marc@smalbrugge.com>
2022-11-22 15:51:59 +0100

    style error page

--- src/components/pages/ErrorPage/index.tsx
+++ src/components/pages/ErrorPage/index.tsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
+import styles from './styles.module.scss';
 
 export const ErrorPage = () => {
     return (
-        <div>
-            <h2>Whoopsie daisy! This page does not exist.</h2>
-            <p>
-                <Link to="/">Go to the home page</Link>
-            </p>
+        <div className={styles.error}>
+            <div className={styles.wrapper}>
+                <h2 className={styles.title}>
+                    <div>Whoopsie daisy!</div>
+                    This page does not exist.
+                </h2>
+
+                <p className={styles.text}>
+                    <Link to="/">Go to the home page</Link>
+                </p>
+            </div>
         </div>
     );
 };
